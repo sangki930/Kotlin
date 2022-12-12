9bad84028ac9b768ec68d3ffcd442bf75414bbcd
sangki930 <sangki931@gmail.com>
2022-12-12 11:04:33 +0900

    Test Commit

--- Day01/src/main/kotlin/com/basic/InputAndOutput.kt
+++ Day01/src/main/kotlin/com/basic/InputAndOutput.kt
@@ -1,7 +1,7 @@
 package com.basic
 
 fun main(){
-    // val : (함수 main에서 쓰는) 지역변수(local variable)
+    // val : (함수 main()에서 쓰는) 지역변수(local variable)
     val a = readLine()!!.toInt() // a를 입력받고 Int로 전환
     val b = readLine()!!.toInt() // b를 입력받고 Int로 전환
     println(a+b)
