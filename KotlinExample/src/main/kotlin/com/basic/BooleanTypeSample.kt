1bd9392ddebd1c8671e9a728472486c70d9d2065
sangki930 <sangki931@gmail.com>
2022-12-12 16:09:52 +0900

    Test Commit

--- KotlinExample/src/main/kotlin/com/basic/BooleanTypeSample.kt
+++ KotlinExample/src/main/kotlin/com/basic/BooleanTypeSample.kt
@@ -2,4 +2,7 @@ fun main(){
      // ! : 논리 부정
      // or, and, xor => 즉시 계산(eager) 방식의 논리합
      // ||, &&  => 지연 계산(lazy) 방식의 논리합, 논리곱
+
+    println(true or false)
+    println(1==1 || 2>3)
 }
--- KotlinExample/src/main/kotlin/com/basic/StringSample.kt
+++ KotlinExample/src/main/kotlin/com/basic/StringSample.kt
@@ -2,5 +2,7 @@ package com.basic
 
 fun main(){
     var s1 = "Hello!"
-    
+    var s2 = s1+"World!!"
+
+    println(s2)
 }
