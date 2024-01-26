70c43068021d6a5e339d5a74356669ad791b92a7
sangki930 <sangki931@gmail.com>
2024-01-26 22:56:25 +0900

    Day06커밋

---
+++ KotlinExample/src/main/kotlin/programmers/basic/Day06/문자열뒤집기/Solution.kt
@@ -0,0 +1,8 @@
+package programmers.basic.Day06.문자열뒤집기
+
+class Solution {
+    fun solution(my_string: String): String {
+        var answer: String = my_string.reversed()
+        return answer
+    }
+}
