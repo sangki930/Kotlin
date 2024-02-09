70dc19015da81c99059f7f3f347809887bb4486d
sangki930 <sangki931@gmail.com>
2024-02-09 23:56:53 +0900

    간단한 논리연산

---
+++ KotlinExample/src/main/kotlin/programmers/basic/Day08/간단한논리연산/Solution.kt
@@ -0,0 +1,7 @@
+package programmers.basic.Day08.간단한논리연산
+
+class Solution {
+    fun solution(x1: Boolean, x2: Boolean, x3: Boolean, x4: Boolean): Boolean {
+        return (x1 || x2) && (x3 || x4)
+    }
+}
