0426016744286506e52de92b8d715c39df244982
sangki930 <sangki931@gmail.com>
2024-02-18 22:56:15 +0900

    코딩기초트레이닝 Day04

--- KotlinExample/src/main/kotlin/programmers/basic/Day04/배열의평균값/Solution.kt
+++
@@ -1,7 +0,0 @@
-package programmers.basic.Day04.배열의평균값
-
-class Solution {
-    fun solution(numbers: IntArray): Double {
-        return numbers.average()
-    }
-}
---
+++ KotlinExample/src/main/kotlin/programmers/basic/Day04/피자나눠먹기/_03/Solution.kt
@@ -0,0 +1,5 @@
+package programmers.basic.Day04.피자나눠먹기._03
+
+class Solution {
+    fun solution(slice: Int, n: Int)= Math.ceil((n*1.0) / slice)
+}
