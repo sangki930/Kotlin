eb3c3f55ecfb5fbcfdff8d5d994305d6e57ee4cb
sangki930 <sangki931@gmail.com>
2024-03-11 23:10:55 +0900

    코딩 입문 트레이닝  Day21

--- KotlinExample/src/main/kotlin/kotlinexample/whenexample/protractor/Solution.kt
+++ KotlinExample/src/main/kotlin/kotlinexample/whenexample/protractor/Solution.kt
@@ -1,7 +1,8 @@
 package kotlinexample.whenexample.protractor
 
 // 출처 : https://school.programmers.co.kr/learn/courses/30/lessons/120829
-class Solution {
+class
+shpsSolution {
     fun solution(angle: Int): Int {
         return when{
             angle in 1 until 90 -> 1 // 예각
