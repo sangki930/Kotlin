39adb9ac43126437803401ec19f4bf2394e8d687
sangki930 <sangki931@gmail.com>
2024-06-11 23:55:53 +0900

    job(1)

---
+++ KotlinExample/src/main/kotlin/kotlinexample/coroutineexample/job/Example.kt
@@ -0,0 +1,19 @@
+package kotlinexample.coroutineexample.job
+
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main(){
+
+    runBlocking {
+        val job = launch (start = CoroutineStart.LAZY) {
+            println("job started");
+        }
+        delay(1000)
+
+        println("Preparing to start...")
+        job.start();
+    }
+}
